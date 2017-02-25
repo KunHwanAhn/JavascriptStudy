667dd0bcfc6ac3d455bf096360151fd2d777b68a
Conan Kunhwan Ahn <kunhwon@gmail.com>
2017-02-25 13:18:01 +0900

    17-02-18 Study

---
+++ 17-02-18/Chapter2/battleship.js
@@ -0,0 +1,32 @@
+var LOCATION1 = 3,
+    LOCATION2 = 4,
+    LOCATION3 = 5;
+
+var guess,
+    hits = 0,
+    guesses = 0,
+    stats;
+
+var isSunk = false;
+
+while (isSunk === false) {
+  guess = parseInt(prompt('준비, 조준, 발사! (0에서 6까지 중 추측한 숫자를 입력하세요):'), 0);
+
+  if (guess < 0 || guess > 6) {
+    alert('Invalid Input');
+  } else {
+    guesses++;
+
+    if (guess === LOCATION1 || guess === LOCATION2 || guess === LOCATION3) {
+      hits++;
+
+      if (hits === 3) {
+        isSunk = true;
+        alert('전함 침몰!');
+      }
+    }
+  }
+}
+
+stats = '총 ' + guesses + '번 발사했습니다. 명중률: ' + (3 / guesses);
+alert(stats);
